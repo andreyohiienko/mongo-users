8cc6a9544dc47872ccc49e93e8594c226e9de086
Andrei Ohiienko <andreyohiienko@gmail.com>
2020-09-09 20:55:05 +0300

    Set and save for updataing records

--- test/update_test.js
+++ test/update_test.js
@@ -9,5 +9,17 @@ describe('Updating records', () => {
     joe.save().then(() => done())
   })
 
-  it('instance type using set n save', () => {})
+  it('instance type using set n save', (done) => {
+    // Possible to make few changes and only then call save()
+    // in order touching data base one time
+    joe.set('name', 'Alex')
+    joe
+      .save()
+      .then(() => User.find({}))
+      .then((users) => {
+        assert(users.length === 1)
+        assert(users[0].name === 'Alex')
+        done()
+      })
+  })
 })
