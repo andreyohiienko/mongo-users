45747c65b6a16344cd453349aa0b0f7b0b3dc26f
Andrei Ohiienko <andreyohiienko@gmail.com>
2020-09-09 18:35:35 +0300

    Default promise implementation

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,14 +1,20 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost/users_test', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-mongoose.connection
-  .once('open', () => console.log('Good to go'))
-  .on('error', () => {
-    console.warn('Warning', error)
+mongoose.Promise = global.Promise
+
+before((done) => {
+  mongoose.connect('mongodb://localhost/users_test', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
   })
+  mongoose.connection
+    .once('open', () => {
+      done()
+    })
+    .on('error', () => {
+      console.warn('Warning', error)
+    })
+})
 
 beforeEach((done) => {
   mongoose.connection.collections.users.drop(() => {
