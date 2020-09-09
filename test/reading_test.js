db737706a22914504830ee01f83c72aff10324e2
Andrei Ohiienko <andreyohiienko@gmail.com>
2020-09-09 18:50:17 +0300

    Making mongo queries

--- test/reading_test.js
+++ test/reading_test.js
@@ -4,10 +4,15 @@ const User = require('../src/user')
 describe('Reading users out of the database', () => {
   let joe
 
-  beforeEach(() => {
+  beforeEach((done) => {
     joe = new User({ name: 'Joe' })
     joe.save().then(() => done())
   })
 
-  it('should find all users with a name of joe', () => {})
+  it('should find all users with a name of joe', (done) => {
+    User.find({ name: 'Joe' }).then((users) => {
+      console.log(users)
+      done()
+    })
+  })
 })
