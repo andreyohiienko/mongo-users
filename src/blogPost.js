556ca1d47710fe03660bb33a5a931ed077c1d078
Andrei Ohiienko <andreyohiienko@gmail.com>
2020-09-11 14:00:44 +0300

    Creating associations with refs

--- src/blogPost.js
+++ src/blogPost.js
@@ -4,6 +4,9 @@ const Schema = mongoose.Schema
 const BlogPostSchema = new Schema({
   title: String,
   content: String,
+  comments: [{ type: Schema.Types.ObjectId, ref: 'Comment' }],
 })
 
-module.exports = BlogPostSchema
+const BlogPost = mongoose.model('blogPost', BlogPostSchema)
+
+module.exports = BlogPost
