53f5f89577dd32a452ad4955f1598f953ac15917
Andrei Ohiienko <andreyohiienko@gmail.com>
2020-09-11 20:32:49 +0300

    Writing skip and limit queries

--- test/reading_test.js
+++ test/reading_test.js
@@ -2,12 +2,19 @@ const assert = require('assert')
 const User = require('../src/user')
 
 describe('Reading users out of the database', () => {
-  let joe
+  let joe, maria, alex, zach
 
   beforeEach((done) => {
     // use 'done' for async code
+
+    alex = new User({ name: 'Alex' })
     joe = new User({ name: 'Joe' })
-    joe.save().then(() => done())
+    maria = new User({ name: 'Maria' })
+    zach = new User({ name: 'Zach' })
+
+    Promise.all([alex.save(), joe.save(), maria.save(), zach.save()]).then(() =>
+      done(),
+    )
   })
 
   it('should find all users with a name of joe', (done) => {
@@ -25,5 +32,10 @@ describe('Reading users out of the database', () => {
       assert(user.name === 'Joe')
       done()
     })
+  })
+
+  it('can skip and limit the result set', () => {
+    // -Alex- [Joe Maria] Zach
+    User.find({}).skip(1).limit(2)
   })
 })
